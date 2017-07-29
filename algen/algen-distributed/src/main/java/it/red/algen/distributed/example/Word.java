cf40388e91731cfe7b99a0f8832092531ec6b19e
eltanin3 <eltanin3@hotmail.it>
2017-07-29 23:32:31 +0200

    Added distributed Spark-powered module: only sample code

---
+++ algen/algen-distributed/src/main/java/it/red/algen/distributed/example/Word.java
@@ -0,0 +1,21 @@
+package it.red.algen.distributed.example;
+
+public class Word {
+
+    private String word;
+
+    public Word() {
+    }
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
