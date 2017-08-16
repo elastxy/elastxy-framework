c7f39344ad760efc015e4665ca64e869889431f4
eltanin3 <eltanin3@hotmail.it>
2017-08-17 00:02:19 +0200

    Sudoku fitness refinements

--- algen/algen-applications/src/main/java/it/red/algen/metasudoku/MesIncubator.java
+++ algen/algen-applications/src/main/java/it/red/algen/metasudoku/MesIncubator.java
@@ -1,7 +1,11 @@
 package it.red.algen.metasudoku;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import it.red.algen.domain.genetics.Gene;
 import it.red.algen.domain.genetics.NumberPhenotype;
@@ -32,8 +36,19 @@ public class MesIncubator implements Incubator<SequenceGenotype, NumberPhenotype
 	}
 	
 	
-	private int countCompleteRowsSquares(int[][] matrix){
-		int totalSuccessful = 0;
+	/**
+	 * Fitness raw for every row/column/square:
+	 *  - 1.0: if completed
+	 *  OR
+	 * 	- 0.05: for every distinct value beyond 1
+	 * 	OR
+	 *  TODOM- 0.01: if sum is 44,45,46
+	 * 
+	 * @param matrix
+	 * @return
+	 */
+	private double countCompleteRowsSquares(int[][] matrix){
+		double totalPoints = 0;
 		
 		// Rows
 		for(int c=0; c < matrix.length; c++){
@@ -42,31 +57,49 @@ public class MesIncubator implements Incubator<SequenceGenotype, NumberPhenotype
 				column[r] = matrix[r][c];
 			}
 			Arrays.sort(column);
-			if(Arrays.equals(column, COMPLETED_VALUES)) totalSuccessful++;
+			totalPoints = calculateElementPoints(totalPoints, column);
 		}
 		
 		// Columns
 		for(int r=0; r < matrix.length; r++){
 			int[] row = matrix[r].clone();
 			Arrays.sort(row);
-			if(Arrays.equals(row, COMPLETED_VALUES)) totalSuccessful++;
+			totalPoints = calculateElementPoints(totalPoints, row);
 		}
 		
 		
 		// Squares
-		for(int s=0; s < 3; s++){
-			int[] values = new int[9];
-			int i=0;
-			for(int r=s*3; r < s*3+3; r++){
-				for(int c=s*3; c < s*3+3; c++){
-					values[i++] = matrix[r][c];
+		
+		// maxi row
+		for(int sr=0; sr < 3; sr++){
+			
+			// maxi column
+			for(int sc=0; sc < 3; sc++){
+
+				// 3x3 square
+				int[] values = new int[9];
+				int i=0;
+				for(int r=sr*3; r < sr*3+3; r++){
+					for(int c=sc*3; c < sc*3+3; c++){
+						values[i++] = matrix[r][c];
+					}
 				}
+				Arrays.sort(values);
+				totalPoints = calculateElementPoints(totalPoints, values);
 			}
-			Arrays.sort(values);
-			if(Arrays.equals(values, COMPLETED_VALUES)) totalSuccessful++;
 		}
 		
-		return totalSuccessful;
+		return totalPoints;
+	}
+
+	private double calculateElementPoints(double totalPoints, int[] values) {
+		if(Arrays.equals(values, COMPLETED_VALUES)) totalPoints++;
+		else {
+			Set<Integer> ints = Arrays.stream(values).boxed().collect(Collectors.toSet());
+			int distinct = ints.size();
+			totalPoints += distinct * 0.1;
+		}
+		return totalPoints;
 	}
 
 }
