e4c58461ee55a0ad64d104f51a5400352e0e1dc6
eltanin3 <eltanin3@hotmail.it>
2018-01-24 22:21:48 +0100

    Fix to SHA-256 password: must be lower case to be successfully checked.

--- algen/elastxy-web/src/main/resources/sql-scripts/data.sql
+++ algen/elastxy-web/src/main/resources/sql-scripts/data.sql
@@ -3,8 +3,8 @@ INSERT INTO app_role (id, role_name, description) VALUES (2, 'ADMIN_USER', 'Admi
 
 -- USER
 -- non-encrypted password: secretxy
-INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (1, 'AppUser', 'AppUser', '0533FB5ABC04E6A46189314E480452E79B1B668E25A1079EDF1D14BB0CDB0687', 'appuser');
-INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (2, 'AdminUser', 'AdminUser', '0533FB5ABC04E6A46189314E480452E79B1B668E25A1079EDF1D14BB0CDB0687', 'adminuser');
+INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (1, 'AppUser', 'AppUser', '0533fb5abc04e6a46189314e480452e79b1b668e25a1079edf1d14bb0cdb0687', 'appuser');
+INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (2, 'AdminUser', 'AdminUser', '0533fb5abc04e6a46189314e480452e79b1b668e25a1079edf1d14bb0cdb0687', 'adminuser');
 
 
 INSERT INTO user_role(user_id, role_id) VALUES(1,1);
