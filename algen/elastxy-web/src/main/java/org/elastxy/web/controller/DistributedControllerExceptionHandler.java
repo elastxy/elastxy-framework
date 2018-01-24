d6590b65be68ee9a164791664e9b11c9189d0128
eltanin3 <eltanin3@hotmail.it>
2018-01-24 22:42:12 +0100

    Added Spring Security exceptions to Handler.

--- algen/elastxy-web/src/main/java/org/elastxy/web/controller/DistributedControllerExceptionHandler.java
+++ algen/elastxy-web/src/main/java/org/elastxy/web/controller/DistributedControllerExceptionHandler.java
@@ -11,6 +11,8 @@ import org.elastxy.core.engine.core.IllegalSolutionException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -33,7 +35,7 @@ public class DistributedControllerExceptionHandler extends ResponseEntityExcepti
 	 */
 
 	// WRONG AUTHENTICATION
-	@ExceptionHandler(value = { SecurityException.class })
+	@ExceptionHandler(value = { SecurityException.class, AuthenticationException.class })
     protected ResponseEntity<Object> handleAuthentication(RuntimeException ex, WebRequest request) {
     	String bodyOfResponse = "Security issue detected (authentication or generic).";
     	log(bodyOfResponse, ex);
@@ -41,7 +43,7 @@ public class DistributedControllerExceptionHandler extends ResponseEntityExcepti
     }
 	
 	// ACCESS TO RESOURCES NOT ALLOWED
-    @ExceptionHandler(value = { AccessControlException.class })
+    @ExceptionHandler(value = { AccessControlException.class, AccessDeniedException.class })
     protected ResponseEntity<Object> handleAuthorization(RuntimeException ex, WebRequest request) {
     	String bodyOfResponse = "Security issue detected (authorization on resources denied).";
     	log(bodyOfResponse, ex);
