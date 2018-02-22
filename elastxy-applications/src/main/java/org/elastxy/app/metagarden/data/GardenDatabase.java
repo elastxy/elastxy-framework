cd0db7cac82191a822e7b10bc807395542635fb5
eltanin3 <eltanin3@hotmail.it>
2018-02-22 16:49:04 +0100

    Detached elastxy-applications submodule.

--- elastxy-applications/src/main/java/org/elastxy/app/metagarden/data/GardenDatabase.java
+++
@@ -1,9 +0,0 @@
-package org.elastxy.app.metagarden.data;
-
-public interface GardenDatabase {
-		
-	public Tree[] getAllTrees();
-	
-	public Place[] getAllPlaces();
-	
-}
--- elastxy-applications/src/main/java/org/elastxy/app/metagarden/data/PlaceProperty.java
+++
@@ -1,6 +0,0 @@
-package org.elastxy.app.metagarden.data;
-
-// TODO3-4: meg: schemaless: infer from source (e.g. CSV sheet headers)
-public enum PlaceProperty {
-	SUN_EXPOSURE, WET_LEVEL, WIND_LEVEL;
-}
